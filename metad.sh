6138a9f807961c6bdb09c8da9ff61ab6a4904389
Ors1mer <ors1mer@ors1mer.xyz>
2024-04-12 13:24:01 +0300

    update the launch script for linux

--- metad.sh
+++ metad.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-python "$(dirname "$0")/main.py" "$@"
+#!/bin/sh
+# assuming the script is in the program directory
+SRC="$(dirname "$0")"/main.py 
+PYTHON=/usr/bin/python3
+$PYTHON $SRC "$@"
